bdf93c0e7f6bc447caac401345c6797cec629102
odelldavisiii <odelldavisiii@192.168.1.128>
2019-06-20 21:34:04 -0400

    Added SpringBootServletInitializer

--- hms/src/main/java/com/davis/hms/HmsApplication.java
+++ hms/src/main/java/com/davis/hms/HmsApplication.java
@@ -2,10 +2,17 @@ package com.davis.hms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HmsApplication {
+public class HmsApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HmsApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HmsApplication.class, args);
 	}
